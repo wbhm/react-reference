f08eeefdc28526057c00dda07b792d71d60fca97
wbhm <wbhmoore@gmail.com>
2018-08-09 12:10:57 -0500

    added test for app store

---
+++ src/store/store.test.js
@@ -0,0 +1,25 @@
+import expect from 'expect';
+import { createStore } from 'redux';
+import rootReducer from '../reducers';
+import initialState from '../reducers/initialState';
+import * as courseActions from '../actions/courseActions';
+
+describe('Testing App Store', () => {
+    it('should handle creating courses', () => {
+        //arrange
+        const store = createStore(rootReducer, initialState);
+        const course = {
+            title: 'TEST COURSE'
+        }
+
+        //act
+        const action = courseActions.createCourseSuccess(course);
+        store.dispatch(action);
+
+        //assert
+        const actual = store.getState().courses[0];
+        //expect(actual.length).toEqual(1);
+        expect(actual).toEqual(course);
+
+    })
+}); 
